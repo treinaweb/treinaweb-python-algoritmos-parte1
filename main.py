db8b441a8914d2bd8d4e7c827ae3abe63415f2fe
fagnerpsantos <fagnerpinheirosantos@gmail.com>
2019-07-26 09:46:22 -0300

    03.01_Entendendo_Implementando_Busca_Linear

--- main.py
+++ main.py
@@ -9,3 +9,17 @@ for i in range(tamanho):
     numeros.append(valor)
 print("Vetor: ", numeros)
 print("Posição 1", numeros[1])
+
+# BUSCA LINEAR
+numero_pesquisar = int(input("Digite o valor a ser pesquisado no vetor: ")) #2
+posicao_resultado = -1
+for i in range(tamanho):
+    # 0 .. 5
+    if numeros[i] == numero_pesquisar:
+        posicao_resultado = i
+        break
+if posicao_resultado < 0:
+    print("O elemento não foi encontrado no vetor")
+else:
+    print(f"Elemento encontrado na posição {posicao_resultado}")
+# FIM BUSCA LINEAR
