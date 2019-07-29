6ce611a1b9096d5c8157aa2f836ac481fd11e752
fagnerpsantos <fagnerpinheirosantos@gmail.com>
2019-07-29 10:34:10 -0300

    05.02_Entendendo_Implementando_Busca_Binaria_PT2

--- main.py
+++ main.py
@@ -42,3 +42,29 @@ for i in range(tamanho):
     print("vetor:", numeros)
 
 # FIM SELECTION SORT
+
+# BUSCA BINÁRIA
+# 2
+# 1 | 2 | 4 | 5 | 6
+# 1 | 2
+# 2
+
+resultado = -1
+inicio = 0
+fim = tamanho - 1
+meio = 0
+alvo = int(input("Digite o elemento a ser encontrado: "))
+while inicio <= fim:
+    meio = int((inicio + fim) / 2)
+    if (numeros[meio] < alvo):
+        inicio = meio + 1
+    elif (numeros[meio] > alvo):
+        fim = meio - 1
+    else:
+        resultado = meio
+        break
+if resultado < 0:
+    print("Elemento não encontrado")
+else:
+    print(f"O elemento {alvo} está na posição {resultado}")
+# FIM BUSCA BINÁRIA
